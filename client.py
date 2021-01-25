053a7aba39993bff5fd99d52d9fbf4a206cca343
Sam Sebastian <sebastiansam55@gmail.com>
2021-01-24 20:27:53 -0500

    testing commit

--- client.py
+++ client.py
@@ -1,12 +1,16 @@
 import asyncio
 import websockets
+import sys
+
+
 
 async def hello():
     uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
-        await websocket.send("Connected")
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
+        while True:
+            greeting = await websocket.recv()
+            print(f"< {greeting}")
+        
 
 asyncio.get_event_loop().run_until_complete(hello())
 asyncio.get_event_loop().run_forever()
