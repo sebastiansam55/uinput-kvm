1c96840d53e0641a3d76a7a62bc7e518ec2caced
Sam Sebastian <sebastiansam55@gmail.com>
2021-01-25 00:56:01 -0500

    Working with one client. work needed to support swapping between and other features. Need sleep.

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
 # uinput-kvm
-WIP uinput KVM
+UInput "KVM". Note that the KVM portion is in quotes! 10ms lag from computer to VM.
+
+## TODO
+Have to add some way for the server to distinguish between who is sending things and who they should be sent to.
+
+Probably going to have clients identify with a name (`--name`) and local devices identify similarly via the first info sent to the server.
+
+Have to add some way to swap between what client is sent the info. Probably going to modify `event_loop` and have it send the name of the device the events should be sent to. And use the same loop to listen for hotkeys to change the client being sent to. 
+
+## Limitations/Notes
+WebSockets are alright. I have yet to test this on anything but a single client local VM back and forth.
+
+Because of the implementation you can't really connect from your own IP address, I guess this would extend to WAN applications? Not sure. I wouldn't recommend using something like this on anything but a local network for both latency and security issues.
+
+I haven't done extensive (read any) testing on what is gonna happen when the devices that are being listened to are lost or disconnected (unplug/reboot). My guess would be that the program wouldn't like it. 
+
+Also I'm not even sure how accurate the quoted 10ms lag time is because I have also been able to get negative numbers from the `<timestamp sent>-time.time()`.
+
+The program only grabs devices via paths right now so that might mean that they could change between reboots. I will probably implement an alternate means of selection later. 
 
 
 ## Installation
 `pip install -r requirements.txt`
 
-In order for the program to run correctly it needs to have access to the `uinput` system in Linux. This can be accomplished either by just using `sudo` everytime you run the program (for both host and clients!) or by adding the user to the `input` group. The latter process is documented below.
+In order for the program to run correctly it needs to have access to the `uinput` system in Linux. This can be accomplished either by just using `sudo` every time you run the program (for both host and clients!) or by adding the user to the `input` group. The latter process is documented below.
 
 For the `evdev` package to function correctly the user running the command has to be in the `input` user group. You can check what groups your user is in with `groups <username>`. 
 
@@ -14,4 +32,59 @@ You can add a user to a group like so:
 
 The documentation for the `evdev` packages states that it is "typically" the `input` group, if this is not the case I wish you luck. 
 
+I would also recommend a full restart to ensure that the user group changes take effect.
+
+I also had to `chmod 660 /dev/uinput` on my testing VM and reboot again. Not quite sure why.
+
+I'm not entirely sure that the second part will be necessary, it may have been something in the code that was misbehaving
+
 ## Usage
+Dang so there are alot of options huh. We're going to start from the top and work our way down the options. 
+
+| Flag | Description | [S]erver/[C]lient/[O]ptional Flag |
+| `-c, --client` | The IP address the "remote" machine should connect to. | [C] |
+| `-n --name` | The name of the client to transmit to the server. Unused currently | [CO] |
+| `-s --server` | Address for the server to bind to (typically found with `ipconfig`) | [S] |
+| `-m --mouse` | Path of the mouse device to grab. | [S] |
+| `-k --keyboard` | Path of the keyboard device to grab. | [S] |
+| `-d --dev_name` | Name to use for the new input device | [CO] |
+| `-p --port` | Port to listen/connect to. Default 8765 | [CSO] |
+| `-v --verbose` | Doesn't do anything right now | [CSO] |
+| `-g --grab` | Exclusively grab the devices | [SO] |
+| `-l --list` | Lists the devices connected to the computer. | [standalone] |
+| `--ssl` | Implemented ssl support according to docs. Does not work for me. | [CSO] |
+
+
+## SSL
+In order to enable ssl you need to have a self signed certificate that you can distribute to all of the computers. 
+
+I followed [this](https://serverfault.com/questions/889581/how-to-generate-a-pem-certificate-in-an-easy-way-for-testing) guide to generate `.pem` files. 
+
+`openssl req -new -newkey rsa:4096 -nodes -keyout kvm.key -out kvm.csr`
+
+then 
+
+`openssl x509 -req -sha256 -days 365 -in kvm.csr -signkey kvm.key -out kvm.pem`
+
+In theory at least. I implemented it as described [here](https://websockets.readthedocs.io/en/stable/intro.html#secure-example) but have not been able to get it working for now.
+
+## Run at boot?
+Because it is a one file script you can easily add it to cron on boot. Whether you want to do this is a conversation you should have with yourself before anyone else. If you have conferred and made a decision here is the easiest way to add it:
+
+On the server KVM machine you should make a script that is just your single line launch command. The add this to cron with `crontab -e`
+
+`@reboot /path/to/script`
+
+where your script should look something like this;
+
+`python3 kvm.py -s 192.168.0.22 -p 8765 -m /dev/input/event5 -k /dev/input/event3 -g`
+
+Note that if you use `sudo crontab` you need to make sure all the same python packages the script uses are also installed for the `root` user.
+
+Then on each host machine install another cron job in a similar fashion;
+
+`@reboot /path/to/script`
+
+where your remote scripts should look something like this;
+
+`python3 kvm.py -c 192.168.0.22 -p 8765 `
--- kvm.py
+++ kvm.py
@@ -5,10 +5,12 @@ import logging
 import threading
 import websockets
 from websockets.exceptions import ConnectionClosedError, ConnectionClosedOK
+from asyncio.exceptions import CancelledError
 from queue import Queue
 import time
 import json
-# import sys
+import sys
+import ssl
 
 parser = argparse.ArgumentParser(description="UInput KVM")
 parser.add_argument('-c', '--client', dest="client", action="store", help="Address to connect to")
@@ -17,9 +19,14 @@ parser.add_argument('-s', '--server', dest="server", action="store", help="Addre
 parser.add_argument('-m', '--mouse', dest="mouse", action="store", help="Full path to mouse device")
 parser.add_argument('-k', '--keyboard', dest="keyboard", action="store", help="Full path to keyboard device")
 parser.add_argument('-d', '--dev_name', dest="dev_name", default="UInput KVM", action="store", help="Name of device to be created for remote interactions")
-parser.add_argument('-p', '--port', dest="port", action="store", default="8765", help="Port for server or client to use")
+parser.add_argument('-p', '--port', dest="port", action="store", default="8765", help="Port for server or client to use. Defaults to 8765")
 parser.add_argument('-v', '--verbose', dest="verbose", action="store_true", help="Verbose logging")
-#TODO rip list function from uinput keyboard mapper
+parser.add_argument('-g', '--grab', dest="grab", action="store_true", help="Grab all input from devices")
+parser.add_argument('-ls', '--list', dest="list", action="store_true", help="List device available on computer")
+#TODO add config file reading that maps connecting ip addresses to client names
+# parser.add_argument('--config', dest="config", action="store", help="Config file for hotkey bindings")
+#TODO fix ssl support? Does not work as expected!
+parser.add_argument('--ssl', dest="ssl", default=False, action="store", help="Self signed key file for ssl. (.pem) (also not working for me)")
 
 args = parser.parse_args()
 
@@ -40,17 +47,34 @@ args = parser.parse_args()
 
 #start a server that just passes around messages and keeps track of the clients
 class Server():
-    def __init__(self, address, port):
+    def __init__(self, address, port, ssl_filename):
+        if ssl_filename is None:
+            self.ssl_context = None
+        else:
+            ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+            ssl_context.load_verify_locations(ssl_filename)
+            self.ssl_context = ssl_context
         self.address = address
         self.port = port
         self.clients = set()
+        self.devices = set()
         self.run()
 
     async def connect(self, ws):
-        self.clients.add(ws)
+        if self.address == ws.remote_address[0]:
+            print("Device added: ", ws.remote_address[0])
+            self.devices.add(ws)
+        else:
+            print("Client added: ", ws.remote_address[0])
+            self.clients.add(ws)
 
     async def disconnect(self, ws):
-        self.clients.remove(ws)
+        if self.address == ws.remote_address[0]:
+            print("Device lost: ", ws.remote_address[0])
+            self.devices.remove(ws)
+        else:
+            print("Client lost: ", ws.remote_address[0])
+            self.clients.remove(ws)
 
     async def broadcast(self, message):
         if self.clients:
@@ -80,35 +104,55 @@ class Server():
     def run(self):
         print("Starting server")
         loop = asyncio.new_event_loop()
-        self.server = websockets.serve(self.listen, self.address, int(self.port), loop=loop)
+        self.server = websockets.serve(self.listen, self.address, int(self.port), ssl=self.ssl_context, loop=loop)
         loop.run_until_complete(self.server)
         loop.run_forever()
         
 
 class Client():
     def run(self):
-        loop = asyncio.new_event_loop()
-        loop.run_until_complete(self.event_loop())
-        loop.run_forever()
+        while True:
+            loop = asyncio.new_event_loop()
+            try:
+                loop.run_until_complete(self.event_loop())
+            except CancelledError as ce:
+                print("Connection cancelled")
+            # except:
+            #     e = sys.exc_info()[0]
+            #     print(e)
+            #     print(loop.is_closed())
+            # loop.run_forever()
 
     def get_uri(self):
-        return "ws://"+self.address+":"+self.port
+        if self.ssl:
+            return "wss://"+self.address+":"+self.port
+        else:
+            return "ws://"+self.address+":"+self.port
 
 #HOST that sends out input events
 class HostClient(Client):
-    def __init__(self, address, port, mouse, keyboard):
+    def __init__(self, address, port, ssl_filename, device):
+        if ssl_filename is None:
+            self.ssl=False
+            self.ssl_context=None
+        else:
+            ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+            ssl_context.load_verify_locations(ssl_filename)
+            self.ssl = True
+            self.ssl_context = ssl_context
         self.address = address
         self.port = port
-        self.mouse = mouse
-        self.keyboard = keyboard
+        self.device = device
         self.run()
 
     async def event_loop(self):
         uri = self.get_uri()
-        async with websockets.connect(uri) as websocket:
-            async for ev in self.mouse.async_read_loop():
-                #TODO add configable hotkeys that will swap computers
+        async with websockets.connect(uri, ssl=self.ssl_context) as websocket:
+            # await websocket.send(json.dumps({"name":"device"}))
+            async for ev in self.device.async_read_loop():
+                #TODO add configable hotkeys that will swap computers info is sent to
                 data = {
+                    "timestamp": str(ev.timestamp()),
                     "type": ev.type,
                     "code": ev.code,
                     "value": ev.value
@@ -117,11 +161,20 @@ class HostClient(Client):
                     await websocket.send(json.dumps(data))
                 except ConnectionClosedOK as cco:
                     print("Connection Closed!")
-            
+                # except ConnectionClosedError as cce:
+                #     print("Connection error")
 
 #client that recieves input events and executes them via uinput!
 class RemoteClient(Client):
-    def __init__(self, address, port, dev_name):
+    def __init__(self, address, port, ssl, dev_name):
+        if ssl_filename is None:
+            self.ssl=False
+            self.ssl_context=None
+        else:
+            ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+            ssl_context.load_verify_locations(ssl_filename)
+            self.ssl = True
+            self.ssl_context = ssl_context
         self.address = address
         self.port = port
         self.ui = evdev.UInput(name=dev_name)
@@ -129,10 +182,11 @@ class RemoteClient(Client):
 
     async def event_loop(self):
         uri = self.get_uri()
-        async with websockets.connect(uri) as websocket:
+        print("Connecting to: ", uri)
+        async with websockets.connect(uri, ssl=self.ssl_context) as websocket:
             async for msg in websocket:
                 data = json.loads(msg)
-                print(data)
+                print(time.time()-float(data['timestamp']), data)
                 self.ui.write(data["type"], data["code"], data["value"])
 
 
@@ -152,32 +206,47 @@ def grab_device(devices, path):
 
 print("Getting list of devices")
 devices = get_devices()
+if args.list:
+    for dev in get_devices():
+        print(dev.path, dev.name, dev.phys)
+    sys.exit()
 
 if args.server:
-    #grab mouse
-    if args.mouse:
-        mouse = grab_device(devices, args.mouse)
-        
-    #grab keyboard
-    if args.keyboard:
-        keyboard = grab_device(devices, args.keyboard)
-        
     #create server
     print("Making server thread")
-    server_thread = threading.Thread(target=Server, args=(args.server, args.port))
+    server_thread = threading.Thread(target=Server, args=(args.server, args.port, args.ssl))
+    server_thread.name = "Server Thread"
     print("Starting server thread")
     server_thread.start()
     time.sleep(1)
 
-    print("Making HostClient thread")
-    host_thread = threading.Thread(target=HostClient, args=(args.server, args.port, mouse,keyboard))
-    print("Starting hostclient thread")
-    host_thread.start()
-
+    #grab mouse
+    if args.mouse:
+        mouse = grab_device(devices, args.mouse)
+        if args.grab:
+            mouse.grab()
+        print("Making MouseHostClient thread")
+        mouse_host_thread = threading.Thread(target=HostClient, args=(args.server, args.port, args.ssl, mouse))
+        mouse_host_thread.name = "Mouse Client Thread"
+        print("Starting mousehostclient thread")
+        mouse_host_thread.start()
+        
+    #grab keyboard
+    if args.keyboard:
+        keyboard = grab_device(devices, args.keyboard)
+        if args.grab:
+            keyboard.grab()
+        print("Making KeyboardHostClient thread")
+        keyboard_host_thread = threading.Thread(target=HostClient, args=(args.server, args.port, args.ssl, keyboard))
+        keyboard_host_thread.name = "Keyboard Client Thread"
+        print("Starting keyboardhostclient thread")
+        keyboard_host_thread.start()
+        
 elif args.client:
     #create a uinput device for both mouse and keyboard?
     print("Making RemoteClient thread")
-    remote_thread = threading.Thread(target=RemoteClient, args=(args.client, args.port, args.dev_name))
+    remote_thread = threading.Thread(target=RemoteClient, args=(args.client, args.port, args.ssl, args.dev_name))
+    remote_thread.name = "Remote Thread"
     print("Starting RemoteClient thread")
     remote_thread.start()
     pass
