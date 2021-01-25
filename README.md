e9f4cb2f00719cd226961f0c914b8bf3e1dfe884
Sam Sebastian <sebastiansam55@gmail.com>
2021-01-25 13:50:06 -0500

    Added hotkey support of swapping between clients.
    Added `kvm.json` example config file
    
    Added `--config` file option containing hotkeys that can swap between clients.
    Added `--debug` flag that allows client debugging (prints does not execute sent commands)
    
    Updated readme to reflect changes.

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 # uinput-kvm
-UInput "KVM". Note that the KVM portion is in quotes! 10ms lag from computer to VM.
-
-## TODO
-Have to add some way for the server to distinguish between who is sending things and who they should be sent to.
-
-Probably going to have clients identify with a name (`--name`) and local devices identify similarly via the first info sent to the server.
-
-Have to add some way to swap between what client is sent the info. Probably going to modify `event_loop` and have it send the name of the device the events should be sent to. And use the same loop to listen for hotkeys to change the client being sent to. 
+UInput "KVM". Note that the KVM portion is in quotes!
 
 ## Limitations/Notes
+10ms lag from computer to VM. Looks like about 20-30ms lag over local network (wifi to wired device)
+
+This was created in under 48 hours. Not battle tested.
+
 WebSockets are alright. I have yet to test this on anything but a single client local VM back and forth.
 
 Because of the implementation you can't really connect from your own IP address, I guess this would extend to WAN applications? Not sure. I wouldn't recommend using something like this on anything but a local network for both latency and security issues.
@@ -18,6 +15,8 @@ I haven't done extensive (read any) testing on what is gonna happen when the dev
 Also I'm not even sure how accurate the quoted 10ms lag time is because I have also been able to get negative numbers from the `<timestamp sent>-time.time()`.
 
 The program only grabs devices via paths right now so that might mean that they could change between reboots. I will probably implement an alternate means of selection later. 
+
+Note also that the program broadcasts all commands to all connected clients and just changes one of the values in the payload at allow the client to determine if it needs to write the event.
 
 
 ## Installation
@@ -44,7 +43,7 @@ Dang so there are alot of options huh. We're going to start from the top and wor
 | Flag | Description | Required |
 | :--: | :---------: | :-------------------------------: |
 | `-c, --client` | The IP address the "remote" machine should connect to. | [C] |
-| `-n --name` | The name of the client to transmit to the server. Unused currently | [CO] |
+| `-n --name` | The name of the client to transmit to the server. Used in the config file. Defaults to hostname | [CO] |
 | `-s --server` | Address for the server to bind to (typically found with `ipconfig`) | [S] |
 | `-m --mouse` | Path of the mouse device to grab. | [S] |
 | `-k --keyboard` | Path of the keyboard device to grab. | [S] |
@@ -52,9 +51,51 @@ Dang so there are alot of options huh. We're going to start from the top and wor
 | `-p --port` | Port to listen/connect to. Default 8765 | [CSO] |
 | `-v --verbose` | Doesn't do anything right now | [CSO] |
 | `-g --grab` | Exclusively grab the devices | [SO] |
-| `-l --list` | Lists the devices connected to the computer. | [standalone] |
+| `-ls --list` | Lists the devices connected to the computer. | [standalone] |
+| `--config` | A json file described in the section below. | [CS] |
 | `--ssl` | Implemented ssl support according to docs. Does not work for me. | [CSO] |
+| `--debug` | Debug for clients, operates everything but actually writing events. | [CO] |
 
+Note that there currently isn't much (any) difference between what happens in the `-k` or `-m` flags. They are currently interchangeable.
+
+
+## Config file
+The program takes a `json` file passed to it via `--config`. In this file you can (sort of) define the hotkeys used to swap between kvm clients.
+
+Provided below is the config file I use for testing.
+```json
+{
+    "modifiers": [29, 42, 56],
+    "hotkeys": [
+        ["test", 2],
+        ["test2", 3]
+    ]
+}
+```
+
+The `modifiers` section describes the keys that should be held down in order to trigger the listen for change of KVM client. They are stored as `int` values that `evdev` can understand. This can get a bit confusing as "1" is an int value of `2`. Here is a table that lists the common modifiers and their `int` values. 
+To see all of the keys that `evdev` recognizes you can run the following in your python console;
+Note that the tab completion is a great feature here, use `e.KEY_LEFT` to see all of the modifiers that start with left. In general the names used are self explanatory. Note that they are all described by their "lowercase" versions (e.g. there is a `KEY_PERIOD` not a `KEY_GREATERTHAN`).
+
+```
+from evdev import ecodes as e
+e.KEY
+```
+
+| Key | ecodes | int value |
+| :-: | :----: | :-------: |
+| Left Shift | KEY_LEFTSHIFT | 42 |
+| Right Shift | KEY_RIGHTSHIFT | 54 |
+| Left Ctrl | KEY_LEFTCTRL | 29 |
+| Right Ctrl | KEY_RIGHTCTRL | 97 |
+| Left Alt | KEY_LEFTALT | 56 |
+| Right Alt | KEY_RIGHTALT | 100 |
+| Left Meta | KEY_LEFTMETA | 125 |
+| Right Meta | KEY_RIGHTMETA | 126 |
+
+You are on your own for determining other codes.
+
+The next part of the config file is the `hotkeys` section. This section is a list of lists in the form of `[<clientname>, <ecodes key to swap to them>]`. The `clientname` is going to be either the name you specified for the client with `--name` or the computer's hostname if no `--name` flag was used. The ecodes key should be an `int` corresponding to the key that you want to press in combination with the modifiers (as described above) to swap to sending outputs to.
 
 ## SSL
 In order to enable ssl you need to have a self signed certificate that you can distribute to all of the computers. 
@@ -89,3 +130,8 @@ Then on each host machine install another cron job in a similar fashion;
 where your remote scripts should look something like this;
 
 `python3 kvm.py -c 192.168.0.22 -p 8765 `
+
+## TODO
+Check that mouse stuff is working as expected with regular mice (it seems trackpoint doesn't work great)
+
+Add way to ungrab local devices upon hotkey. Would also need to regrab upon other hotkeys!
--- kvm.py
+++ kvm.py
@@ -1,4 +1,5 @@
 import evdev
+from evdev import ecodes as e
 import asyncio
 import argparse
 import logging
@@ -10,23 +11,27 @@ from queue import Queue
 import time
 import json
 import sys
+import os
 import ssl
 
 parser = argparse.ArgumentParser(description="UInput KVM")
 parser.add_argument('-c', '--client', dest="client", action="store", help="Address to connect to")
-parser.add_argument('-n', '--name', dest="name", action="store", help="Client Name")
+parser.add_argument('-n', '--name', dest="name", default=os.uname()[1], action="store", help="Client Name")
 parser.add_argument('-s', '--server', dest="server", action="store", help="Address to bind to")
+#TODO grab via a different method
 parser.add_argument('-m', '--mouse', dest="mouse", action="store", help="Full path to mouse device")
 parser.add_argument('-k', '--keyboard', dest="keyboard", action="store", help="Full path to keyboard device")
 parser.add_argument('-d', '--dev_name', dest="dev_name", default="UInput KVM", action="store", help="Name of device to be created for remote interactions")
 parser.add_argument('-p', '--port', dest="port", action="store", default="8765", help="Port for server or client to use. Defaults to 8765")
 parser.add_argument('-v', '--verbose', dest="verbose", action="store_true", help="Verbose logging")
+#TODO work out system that allows for non-exclusive grabs
 parser.add_argument('-g', '--grab', dest="grab", action="store_true", help="Grab all input from devices")
 parser.add_argument('-ls', '--list', dest="list", action="store_true", help="List device available on computer")
-#TODO add config file reading that maps connecting ip addresses to client names
-# parser.add_argument('--config', dest="config", action="store", help="Config file for hotkey bindings")
+parser.add_argument('--config', dest="config", action="store", help="Config file for hotkey bindings")
 #TODO fix ssl support? Does not work as expected!
-parser.add_argument('--ssl', dest="ssl", default=False, action="store", help="Self signed key file for ssl. (.pem) (also not working for me)")
+parser.add_argument('--ssl', dest="ssl", action="store", help="Self signed key file for ssl. (.pem) (also not working for me)")
+parser.add_argument('--debug', dest="debug", default=False, action="store_true", help="Enable client debug (will not write events)")
+
 
 args = parser.parse_args()
 
@@ -47,7 +52,7 @@ args = parser.parse_args()
 
 #start a server that just passes around messages and keeps track of the clients
 class Server():
-    def __init__(self, address, port, ssl_filename):
+    def __init__(self, address, port, ssl_filename, name, config):
         if ssl_filename is None:
             self.ssl_context = None
         else:
@@ -58,6 +63,8 @@ class Server():
         self.port = port
         self.clients = set()
         self.devices = set()
+        self.config = config
+        self.sendto = None
         self.run()
 
     async def connect(self, ws):
@@ -81,17 +88,34 @@ class Server():
             print(self.clients)
             await asyncio.wait([client.send(message) for client in self.clients])
 
+    async def sendto_name(self, message, recp):
+        #takes a name an checks the config
+        if self.config.get(recp):
+            print(self.config.get(recp))
+                            
+
     async def listen(self, websocket, path):
         await self.connect(websocket)
         try:
             async for message in websocket:
-                print(message)
+                # print(message)
                 try:
                     data = json.loads(message)
                     print(data)
                 except:
                     print("json error")
-                await self.broadcast(message)
+                if data.get("change_sendto"):
+                    print("Changing sendto from", self.sendto, "to", data.get("change_sendto"))
+                    self.sendto = data.get("change_sendto")
+                elif data.get("ident") is None:
+                    print(data.get("sendto"))
+                    data["sendto"] = self.sendto
+                    await self.broadcast(json.dumps(data))
+                        # await self.sendto_name(message, data.get("sendto"))
+                    # await self.broadcast(message)
+
+                else:
+                    pass
         except ConnectionClosedError as cce:
             print("Connection Lost")
             print(cce)
@@ -110,6 +134,20 @@ class Server():
         
 
 class Client():
+    def __init__(self, address, port, ssl_filename, name):
+        if ssl_filename is None:
+            self.ssl=False
+            self.ssl_context=None
+        else:
+            ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+            ssl_context.load_verify_locations(ssl_filename)
+            self.ssl = True
+            self.ssl_context = ssl_context
+        self.address = address
+        self.port = port
+        self.name = name
+
+
     def run(self):
         while True:
             loop = asyncio.new_event_loop()
@@ -117,9 +155,9 @@ class Client():
                 loop.run_until_complete(self.event_loop())
             except CancelledError as ce:
                 print("Connection cancelled")
-            # except:
-            #     e = sys.exc_info()[0]
-            #     print(e)
+            except:
+                e = sys.exc_info()[0]
+                print(e)
             #     print(loop.is_closed())
             # loop.run_forever()
 
@@ -131,28 +169,47 @@ class Client():
 
 #HOST that sends out input events
 class HostClient(Client):
-    def __init__(self, address, port, ssl_filename, device):
-        if ssl_filename is None:
-            self.ssl=False
-            self.ssl_context=None
-        else:
-            ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-            ssl_context.load_verify_locations(ssl_filename)
-            self.ssl = True
-            self.ssl_context = ssl_context
-        self.address = address
-        self.port = port
+    def __init__(self, address, port, ssl_filename, name, device, config_data):
+        super().__init__(address, port, ssl_filename, name)
+        self.config = config_data
+        self.hotkeys = self.config.get("hotkeys")
         self.device = device
         self.run()
 
     async def event_loop(self):
+        modifiers = self.config.get("modifiers")
+        # print(modifiers)
         uri = self.get_uri()
+        sendto = None
         async with websockets.connect(uri, ssl=self.ssl_context) as websocket:
-            # await websocket.send(json.dumps({"name":"device"}))
+            await websocket.send(json.dumps({"ident":self.name}))
+            held_keys = []
             async for ev in self.device.async_read_loop():
+                # if ev.code in held_mod:
+                if ev.value == 1: #modifier pressed down
+                    held_keys.append(ev.code)
+                elif ev.value == 0: #modifier released!
+                    if ev.code in held_keys:
+                        held_keys.remove(ev.code)
+                print(held_keys)
                 #TODO add configable hotkeys that will swap computers info is sent to
+                all_held = True
+                for key in modifiers:
+                    if key in held_keys:
+                        all_held = True
+                    else:
+                        all_held = False
+                        break
+                if all_held:
+                    print("Key event detected when left [shift alt ctrl] held", ev)
+                    for client in self.hotkeys:
+                        if client[1] == ev.code:
+                            sendto = client[0]
+                            await self.change_sendto(websocket, sendto)
+
                 data = {
                     "timestamp": str(ev.timestamp()),
+                    "sendto": sendto,
                     "type": ev.type,
                     "code": ev.code,
                     "value": ev.value
@@ -163,20 +220,23 @@ class HostClient(Client):
                     print("Connection Closed!")
                 # except ConnectionClosedError as cce:
                 #     print("Connection error")
+    async def change_sendto(self, websocket, sendto):
+        print("Sending to: ", sendto)
+        data = {"change_sendto": sendto}
+        try:
+            await websocket.send(json.dumps(data))
+        except ConnectionClosedOK as cco:
+            print("Connection closed")
+
+#TODO maybe abstract some more to not do hotkey proceesing for mouse
+# class KeyboardClient():
 
 #client that recieves input events and executes them via uinput!
 class RemoteClient(Client):
-    def __init__(self, address, port, ssl, dev_name):
-        if ssl_filename is None:
-            self.ssl=False
-            self.ssl_context=None
-        else:
-            ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-            ssl_context.load_verify_locations(ssl_filename)
-            self.ssl = True
-            self.ssl_context = ssl_context
-        self.address = address
-        self.port = port
+    def __init__(self, address, port, ssl_filename, name, dev_name, config_data=None, debug=False):
+        super().__init__(address, port, ssl_filename, name)
+        self.config = config_data
+        self.debug = debug
         self.ui = evdev.UInput(name=dev_name)
         self.run()
 
@@ -184,10 +244,17 @@ class RemoteClient(Client):
         uri = self.get_uri()
         print("Connecting to: ", uri)
         async with websockets.connect(uri, ssl=self.ssl_context) as websocket:
+            await websocket.send(json.dumps({"ident":self.name}))
             async for msg in websocket:
                 data = json.loads(msg)
                 print(time.time()-float(data['timestamp']), data)
-                self.ui.write(data["type"], data["code"], data["value"])
+                if self.name == data['sendto']:
+                    if not self.debug:
+                        self.ui.write(data["type"], data["code"], data["value"])
+                    else:
+                        print("Write event:", data["type"], data["code"], data["value"])
+                else:
+                    print("Not meant for me!")
 
 
 def get_devices():
@@ -211,10 +278,18 @@ if args.list:
         print(dev.path, dev.name, dev.phys)
     sys.exit()
 
+if args.config:
+    print("Loading list of clients")
+    with open(args.config, 'r') as f:
+        config_data = json.load(f)
+        print(config_data)
+else:
+    config_data = None
+
 if args.server:
     #create server
     print("Making server thread")
-    server_thread = threading.Thread(target=Server, args=(args.server, args.port, args.ssl))
+    server_thread = threading.Thread(target=Server, args=(args.server, args.port, args.ssl, args.name, config_data))
     server_thread.name = "Server Thread"
     print("Starting server thread")
     server_thread.start()
@@ -226,7 +301,7 @@ if args.server:
         if args.grab:
             mouse.grab()
         print("Making MouseHostClient thread")
-        mouse_host_thread = threading.Thread(target=HostClient, args=(args.server, args.port, args.ssl, mouse))
+        mouse_host_thread = threading.Thread(target=HostClient, args=(args.server, args.port, args.ssl, args.name, mouse, config_data))
         mouse_host_thread.name = "Mouse Client Thread"
         print("Starting mousehostclient thread")
         mouse_host_thread.start()
@@ -237,7 +312,7 @@ if args.server:
         if args.grab:
             keyboard.grab()
         print("Making KeyboardHostClient thread")
-        keyboard_host_thread = threading.Thread(target=HostClient, args=(args.server, args.port, args.ssl, keyboard))
+        keyboard_host_thread = threading.Thread(target=HostClient, args=(args.server, args.port, args.ssl, args.name, keyboard, config_data))
         keyboard_host_thread.name = "Keyboard Client Thread"
         print("Starting keyboardhostclient thread")
         keyboard_host_thread.start()
@@ -245,7 +320,7 @@ if args.server:
 elif args.client:
     #create a uinput device for both mouse and keyboard?
     print("Making RemoteClient thread")
-    remote_thread = threading.Thread(target=RemoteClient, args=(args.client, args.port, args.ssl, args.dev_name))
+    remote_thread = threading.Thread(target=RemoteClient, args=(args.client, args.port, args.ssl, args.name, args.dev_name, None, args.debug))
     remote_thread.name = "Remote Thread"
     print("Starting RemoteClient thread")
     remote_thread.start()
